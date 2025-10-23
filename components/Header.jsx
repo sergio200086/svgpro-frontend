f293d2085e44030d7f7a71c0e1259de7a1936442
Sergio Suárez <62405866+sergio200086@users.noreply.github.com>
2025-10-22 22:17:16 -0500

    new colors, new way to filter data

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,12 +3,14 @@ function Header({className =""}) {
   return (
     <header className="my-4.5">
         <div className="flex items-center justify-end md:justify-between mx-auto max-w-screen-xl px-4">
-          <h1 className="px-2">LOGO SVGPRO</h1>
+          <a href="/">
+            <h1 className="px-2">LOGO SVGPRO</h1>
+          </a>
 
           <nav className= {`flex text-center ${className}`}>
               <ul className="flex gap-x-1 md:gap-x-5">
                   <li>
-                    <a href="#" className="transition hover:text-gray-500/75  dark:hover:text-lime-500">
+                    <a href="/" className="transition hover:text-gray-500/75  dark:hover:text-lime-500">
                       Home
                     </a>
                   </li>
--- components/Searcher.jsx
+++ components/Searcher.jsx
@@ -28,52 +28,59 @@ const API_BASE_URL = 'http://localhost:3001/icons/all-icons';
 function Searcher({className=""}) {
   const [searchTerm, setSearchTerm] = useState('')
   const [logos, setLogos] = useState([])
-  const [isLoading, setIsLoading] = useState (false)
-  const debouncedSearchTerm = useDebounce(searchTerm, 500); 
+  const [filteredLogos, setFilteredLogos] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect (()=>{
     const fetchLogos = async ()=>{
-      setIsLoading(true)
-      const query = debouncedSearchTerm.trim()
       try{
-        const apiUrl = `${API_BASE_URL}?q=${query}`;
+        const apiUrl = `${API_BASE_URL}?q=`;
         const response = await fetch(apiUrl); 
         if (!response.ok) throw new Error(`Error in the API: ${response.statusText}`)
         const data = await response.json();
-        console.log(data)
         setLogos(data || [])
+        setFilteredLogos(data)
       } catch(error) {
         console.error("Something went wrong: ", error)
         setLogos([])
-      } finally {
-        setIsLoading(false)
+      } finally{
+        setLoading(false)
       }
     };
     fetchLogos()
-  }, [debouncedSearchTerm]);
+  }, []);
+
+  const handleInputChange = (e) => { 
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm)
+
+    const filteredItems = logos.filter((logo) =>
+      logo.text.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredLogos(filteredItems);
+  }
   
+
+
   return (
     <div className=" ">
-      <div className={`${className}flex items-center mt-10 mx-130  bg-[#0f172a] py-2 px-3 rounded-lg shadow-md border border-slate-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-slate-500 focus-within:border-slate-500 transition-all duration-300`}>
+      <div className={`${className}flex items-center mt-10 mx-130  bg-[#0f172a] py-2 px-3 rounded-lg shadow-md border border-lime-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-lime-700 focus-within:border-lime-700 transition-all duration-300`}>
         <svg  xmlns="http://www.w3.org/2000/svg" width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
         <input 
           type="text" 
           placeholder="Search SVG..." 
           className="flex-grow bg-transparent  text-white placeholder-gray-400 outline-none  px-4 py-2"
           value={searchTerm}
-          onChange={(e)=>setSearchTerm(e.target.value)}
+          onChange={handleInputChange}
         />
       </div>
+      
+      {loading && <p>Loading...</p>}
 
-      {isLoading && (
-          <div className="mt-12 text-center">
-            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-lime-400 mx-auto"></div>
-            <div className="mt-4 text-xl text-lime-400">Loading Icons...</div>
-          </div>
-      )}
-      {!isLoading && logos.length > 0 && (
+      {filteredLogos.length > 0 && (
         <div className="mt-10 grid grid-cols-3 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 gap-5 max-w-7xl mx-auto text-center">
-            {logos.map((logo) => (
+            {filteredLogos.map((logo) => (
                 <div 
                   key={logo.name} 
                   className="flex flex-col items-center justify-center p-4  bg-[#1b2a4e] rounded-xl shadow-lg border border-[#1b2a4e] hover:border-lime-500 transition-colors group duration-300"
@@ -89,22 +96,6 @@ function Searcher({className=""}) {
             
         </div>
       )}
-
-      
-      
-      {!isLoading && logos.length === 0 && searchTerm.length > 0 && (
-          <p className="mt-10 text-xl text-gray-500">
-              We didn't find "{searchTerm}".
-              
-          </p>
-      )}
-
-      {!isLoading && logos.length === 0 && searchTerm.length === 0 && (
-            <p className="mt-10 text-xl text-gray-500">
-                Write to start looking for tour icons.
-            </p>
-      )}
-
     </div>
   )
 }
